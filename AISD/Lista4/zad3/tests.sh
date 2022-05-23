466c1c6c440e679af6e8afecd9de07441e598090
Sandra Szwed <sandra.szwed19@gmail.com>
2022-05-23 23:15:16 +0200

    Dodano Red Black Tree algorytm

---
+++ AISD/Lista4/zad3/tests.sh
@@ -0,0 +1,5 @@
+../programs/gen_rand 10 DISTINCT | ../programs/RB_BST_test RANDOM PRINT
+../programs/gen_rand 10 DISTINCT | ../programs/RB_BST_test ASCENDING PRINT
+
+../programs/gen_rand 50 DISTINCT | ../programs/RB_BST_test RANDOM PRINT
+../programs/gen_rand 50 DISTINCT | ../programs/RB_BST_test ASCENDING PRINT
